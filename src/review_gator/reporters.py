ff92104b743969bd832fbcff5cba000d8bfc177b
Daniel Watkins <daniel.watkins@canonical.com>
2018-12-21 12:33:06 -0500

    Add documentation to review_gator.reporters
    And also move type comments on to the lines they annotate where
    possible.

--- src/review_gator/reporters.py
+++ src/review_gator/reporters.py
@@ -10,23 +10,41 @@ except ImportError:
 
 
 class ReviewGatorReporter(object):
+    """Super-class that ReviewGator reporters should inherit from."""
 
-    def process_data(self, data):
-        # type: (Dict) -> None
+    def process_data(self, data):  # type: (Dict) -> None
+        """Perform reporting with the given data dict."""
         raise NotImplementedError
 
     @classmethod
-    def enabled(cls):
-        # type: () -> bool
+    def enabled(cls):  # type: () -> bool
+        """A bool indicating whether this reporting class should be called."""
         return False
 
 
 class InfluxDBTotalAgeReporter(ReviewGatorReporter):
+    """
+    When enabled, report the sum of the ages of reviews to InfluxDB.
+
+    Will disable itself if either (a) the influxdb library isn't importable,
+    or (b) the REVIEW_GATOR_METRIC_NAME environment variable isn't set.
+
+    The REVIEW_GATOR_METRIC_NAME environment variable is used to determine the
+    metric name under which results will be submitted.
+
+    The following environment variables will be used to configure the InfluxDB
+    client if present (falling back to the client's defaults otherwise):
+
+    * INFLUXDB_HOST
+    * INFLUXDB_PORT
+    * INFLUXDB_USERNAME
+    * INFLUXDB_PASSWORD
+    * INFLUXDB_DATABASE
+    """
 
     influxdb_args = ['host', 'port', 'username', 'password', 'database']
 
-    def __init__(self):
-        # type: () -> None
+    def __init__(self):  # type: () -> None
         self.now = pytz.utc.localize(datetime.datetime.utcnow())
 
         from influxdb import InfluxDBClient, SeriesHelper
@@ -48,28 +66,28 @@ class InfluxDBTotalAgeReporter(ReviewGatorReporter):
 
         self.series_cls = ReviewAgeSeries
 
-    def _determine_total_age_in_seconds(self, data):
-        # type: (Dict) -> float
+    def _determine_total_age_in_seconds(self, data):  # type: (Dict) -> float
+        """Given a review-gator data dict, sum up the review ages."""
         total_age = 0
         for repo in data.values():
             for pr in repo['pull_requests']:
                 total_age += (self.now - pr['date']).total_seconds()
         return total_age
 
-    def _record_event_in_influxdb(self, total_age):
-        # type: (float) -> None
+    def _record_event_in_influxdb(self, total_age):  # type: (float) -> None
+        """Given a total age, push that in to InfluxDB."""
         self.series_cls(total_age=total_age)
         self.series_cls.commit(self.client)
         print(total_age)
 
-    def process_data(self, data):
-        # type: (Dict) -> None
+    def process_data(self, data):  # type: (Dict) -> None
+        """Determine the total age of reviews and push to InfluxDB."""
         total_age = self._determine_total_age_in_seconds(data)
         self._record_event_in_influxdb(total_age)
 
     @classmethod
-    def enabled(cls):
-        # type: () -> bool
+    def enabled(cls):  # type: () -> bool
+        """True if influxdb is importable and we have a metric name."""
         try:
             import influxdb
         except ImportError:
