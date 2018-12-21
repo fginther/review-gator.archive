3eae6f5e7e45adf245e7afee844cb9988a447d2d
Daniel Watkins <daniel.watkins@canonical.com>
2018-12-21 12:17:49 -0500

    Add InfluxDBTotalAgeReporter
    This calculates the total age of all reviews, and pushes it to an
    InfluxDB metric named "review_gator.total_review_age".  It uses
    INFLUXDB_* environment variables to configure the InfluxDB client.

--- src/review_gator/reporters.py
+++ src/review_gator/reporters.py
@@ -1,5 +1,10 @@
+import datetime
+import os
+
+import pytz
+
 try:
-    from typing import Dict
+    from typing import Dict, List, Text
 except ImportError:
     pass
 
@@ -16,4 +21,63 @@ class ReviewGatorReporter(object):
         return False
 
 
-REPORTER_CLASSES = []
+class InfluxDBTotalAgeReporter(ReviewGatorReporter):
+
+    influxdb_args = ['host', 'port', 'username', 'password', 'database']
+
+    def __init__(self):
+        # type: () -> None
+        self.now = pytz.utc.localize(datetime.datetime.utcnow())
+
+        from influxdb import InfluxDBClient, SeriesHelper
+        # Construct tuples for dict creation
+        client_tuples = [(k, os.environ.get('INFLUXDB_{}'.format(k.upper())))
+                         for k in self.influxdb_args]
+        # Strip out any unset environment variables so the InfluxDB defaults
+        # are used
+        client_kwargs = {k: v for k,v in client_tuples if v is not None}
+        self.client = InfluxDBClient(**client_kwargs)
+
+        # Define this class here, where we know that influxdb is importable
+        class ReviewAgeSeries(SeriesHelper):
+
+            class Meta(object):
+                series_name = 'review_gator.total_review_age'
+                fields = ['total_age']
+                tags = []  # type: List[Text]
+
+        self.series_cls = ReviewAgeSeries
+
+    def _determine_total_age_in_seconds(self, data):
+        # type: (Dict) -> float
+        total_age = 0
+        for repo in data.values():
+            for pr in repo['pull_requests']:
+                total_age += (self.now - pr['date']).total_seconds()
+        return total_age
+
+    def _record_event_in_influxdb(self, total_age):
+        # type: (float) -> None
+        self.series_cls(total_age=total_age)
+        self.series_cls.commit(self.client)
+        print(total_age)
+
+    def process_data(self, data):
+        # type: (Dict) -> None
+        total_age = self._determine_total_age_in_seconds(data)
+        self._record_event_in_influxdb(total_age)
+
+    @classmethod
+    def enabled(cls):
+        # type: () -> bool
+        try:
+            import influxdb
+        except ImportError:
+            return False
+        for arg in cls.influxdb_args:
+            if 'INFLUXDB_{}'.format(arg.upper()) not in os.environ:
+                return False
+        return True
+
+
+REPORTER_CLASSES = [InfluxDBTotalAgeReporter]
